b0862e8a3912fa28c797d0042d6b5a0dfb37986e
Alessandro Di Maria <adm@m42.ch>
2023-10-13 09:53:36 +0200

    exclude integration tests that do not work on CI but locally

--- test/sim/loop_test.exs
+++ test/sim/loop_test.exs
@@ -19,23 +19,25 @@ defmodule Ximula.Sim.LoopTest do
     %{simulator_tasks: simulator_tasks, loop_tasks: loop_tasks, loop: loop}
   end
 
+  @tag ci: :skip
   test "runs queue", %{loop: loop} do
     queue = %Queue{name: "test", func: {LoopTest, :sim, [dest: self()]}, interval: 5}
     Loop.clear(loop)
-    Loop.prepare(loop, queue)
+    Loop.add_queue(loop, queue)
     Loop.start_sim(loop)
     Process.sleep(10)
     Loop.stop_sim(loop)
     assert_received(:success)
   end
 
-  # test "handles timeout", %{loop: loop} do
-  #   queue = %Queue{name: "test", func: {LoopTest, :too_long, [dest: self()]}, interval: 5}
-  #   Loop.clear(loop)
-  #   Loop.prepare(loop, queue)
-  #   Loop.start_sim(loop)
-  #   Process.sleep(50)
-  #   Loop.stop_sim(loop)
-  #   assert_received(:success)
-  # end
+  @tag ci: :skip
+  test "handles timeout", %{loop: loop} do
+    queue = %Queue{name: "test", func: {LoopTest, :too_long, [dest: self()]}, interval: 5}
+    Loop.clear(loop)
+    Loop.add_queue(loop, queue)
+    Loop.start_sim(loop)
+    Process.sleep(50)
+    Loop.stop_sim(loop)
+    assert_received(:success)
+  end
 end
